dfa494d92a16b6aa455e4c31f2a94b50a5d566e8
shreezn <asktelugu@gmail.com>
2024-09-06 09:54:53 +0530

    pygame.draw.polygon() = no shape, to be fixed

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    dt = 0
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        player.draw(screen)
+
 
 if __name__ == '__main__':
     main()
